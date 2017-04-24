7a636c03167a0907cad4a170d4204dee9f3bbc35
kishore <mgorti@unomha.edu>
2017-04-24 17:05:48 -0500

    fifth commit

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -6,7 +6,7 @@
     @endif
 	@if (Auth::user()->role==='staff')
 	<h1 align="center"> <a href="{{action ('StaffController@index')}}" style="font-size:30"> Omaha Mentoring for Kids</a></h1>
-    <h2 style="color:red;">Students</h2>
+    <!--<h2 style="color:red;">Students</h2>-->
 	@endif
 	@if (Auth::user()->role==='admin')
 	<h1 align="center"> <a href="{{action ('AdminController@index')}}" style="font-size:30"> Omaha Mentoring for Kids</a></h1>
@@ -28,11 +28,12 @@
     <!-- <a href="http://omk.com/Students/settings" style="font-size:30;"> Settings </a></br></br> -->
     <br></br>
 	 </h2> 
+@endif
+@if (Auth::user()->role==='staff')
 <h3  align="left">
     <a href="{{url('/students/create')}}" class="btn btn-success"> Create Student</a>
 	  
  </h3> 
-@endif
     <hr>
     <table class="table table-striped table-bordered table-hover">
         <thead>
@@ -69,6 +70,49 @@
         </tbody>
 
     </table>
+    @endif
+    @if (Auth::user()->role==='admin')
+<h3  align="left">
+    <a href="{{url('/students/create')}}" class="btn btn-success"> Create Student</a>
+      
+ </h3> 
+    <hr>
+    <table class="table table-striped table-bordered table-hover">
+        <thead>
+        <tr class="bg-info">
+            <th>Name</th>
+            <th>ID</th>
+            <th>Class</th>
+            <th>Address</th>
+            <th>Zip</th>
+            <th>Email</th>
+            <th>Cell_Phone</th>
+            <th style="text-align: center;" colspan="2">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach ($students as $student)
+            <tr>
+                <td>{{ $student->name }}</td>
+                <td>{{ $student->id}}</td>
+                <td>{{ $student->class }}</td>
+                <td>{{ $student->address }}</td>
+                <td>{{ $student->zip }}</td>
+                <td>{{ $student->email}}</td>
+                <td>{{ $student->cell_phone }}</td>
+                <td><a href="{{route('students.edit',$student->id)}}" class="btn btn-warning">Update</a></td>
+                <td>
+                    {!! Form::open(['method' => 'DELETE', 'route'=>['students.destroy', $student->id]])!!}
+                        {!! Form::submit('Delete', ['class' => 'btn btn-danger','name'=>'remove_levels']) !!}
+                    {!! Form::close() !!}
+                </td>
+            </tr>
+        @endforeach
+
+        </tbody>
+
+    </table>
+    @endif
       @if (Auth::user()->role==='staff')
        @include('stdprnt')
 @endif
